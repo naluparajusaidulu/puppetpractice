f34fc285e4828991aa2753d62f642a812dae471f
unknown <=mrsaidulu244@gmail.com>
2017-04-06 20:20:49 +0530

    install tomcat

---
+++ tomcat/manifests/install.pp
@@ -0,0 +1,15 @@
+# Class:tomcat::install
+#
+#
+class tomcat::install {
+    # resources
+$pack_name = $facts['os']['family'] ? {
+    'RedHat' => 'tomcat',
+    'Debian' => 'tomcat7',
+}
+
+package { $pack_name:
+    ensure => installed,
+    
+}
+}
